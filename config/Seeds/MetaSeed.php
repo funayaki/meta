32e4ee2d095b86184aaaa769ce5edb4295a6d955
Rachman Chavik <rchavik@xintesa.com>
2016-10-07 15:42:07 +0700

    Rename seed files

---
+++ config/Seeds/MetaSeed.php
@@ -0,0 +1,19 @@
+<?php
+namespace Croogo\Install\Config\Data;
+
+class MetaData
+{
+
+    public $table = 'meta';
+
+    public $records = [
+        [
+            'id' => '1',
+            'model' => 'Node',
+            'foreign_key' => '1',
+            'key' => 'meta_keywords',
+            'value' => 'key1, key2',
+            'weight' => ''
+        ],
+    ];
+}
